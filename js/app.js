c1a6eb673d548f6e3e3a4b4a73cfa7061250372e
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2024-01-22 20:04:27 -0300

    Responsivity added in the project

--- js/app.js
+++ js/app.js
@@ -220,4 +220,10 @@ toggle_btn.addEventListener("click", () => {
 
 hamburger.addEventListener("click", () => {
     document.body.classList.toggle("open");
-})
+    document.body.classList.toggle("stopScrolling");
+});
+
+links.forEach((link) => link.addEventListener("click", () => {
+    document.body.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+}))
